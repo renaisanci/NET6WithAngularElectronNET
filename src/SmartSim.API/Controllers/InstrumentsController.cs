0671054ddac315702054e89cc7010ccbeaf0a0fc
fonser12 <fonser12@RTCMW031930>
2022-09-14 13:43:47 +0200

    tweaks regading controller names

--- src/SmartSim.API/Controllers/InstrumentsController.cs
+++ src/SmartSim.API/Controllers/InstrumentsController.cs
@@ -4,16 +4,16 @@ namespace SmartSim.UI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class InstrumentsController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<InstrumentsController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public InstrumentsController(ILogger<InstrumentsController> logger)
         {
             _logger = logger;
         }
